2a1c578b6e4779c8043153e73fc9c2832ce71a81
Henry Snopek <hhsnopek@gmail.com>
2017-12-06 13:09:10 -0500

    fix example formatting

--- doc.go
+++ doc.go
@@ -4,29 +4,28 @@
 //
 // The following is a complete example of a http.Handler
 //    import (
-//			"net/http
-//			"encoding/json"
+//      "net/http
+//      "encoding/json"
 //
-//			"github.com/hhsnopek/etag"
+//      "github.com/hhsnopek/etag"
 //    )
 //
 //    type Foo struct {
-//			Bar string `json:"bar"`
-//		}
+//      Bar string `json:"bar"`
+//    }
 //
 //    func FooHandler(w http.ResponseWriter, r *http.Request) {
-//			body, err := json.Marshal(&Foot{Bar: "baz"})
-//			if err != nil {
-//				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-//				return
-//			}
+//      body, err := json.Marshal(&Foot{Bar: "baz"})
+//      if err != nil {
+//        http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+//        return
+//      }
 //
-// 			etag := etag.Generate(body, true)
-//			w.Header().Set("Content-Type", "application/json")
-//			w.Header().Set("Etag", etag)
-//			w.WriteHeader(http.StatusOK)
-//			w.Write(body)
-//			return
-//
+//      etag := etag.Generate(body, true)
+//      w.Header().Set("Content-Type", "application/json")
+//      w.Header().Set("Etag", etag)
+//      w.WriteHeader(http.StatusOK)
+//      w.Write(body)
+//      return
 //    }
 package etag
